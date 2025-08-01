afa6b51b1bdb2765a7a82d21747c9da3c4411432
motoyocodes <motoyosilove@gmail.com>
2025-08-01 18:18:22 +0100

    done

--- src/components/homepage.tsx
+++ src/components/homepage.tsx
@@ -3,7 +3,7 @@ function Homepage() {
     <div className="flex justify-center items-center w-screen h-screen bg-yellow-400">
       <div className="card">
         <div>
-          <img className="image" src="/images/image1.webp" alt="" />
+          <img className="image" src="src/assets/images/image1.webp" alt="" />
           <p className="learn">Learning</p>
           <p>Published 21 Dec 2013</p>
           <h1 className="font-bold">HTML and CSS Foundations</h1>
@@ -15,7 +15,7 @@ function Homepage() {
           <div className="flex items-center gap-2 mt-2">
             <img
               className="w-10 h-10 rounded-2xl "
-              src="public/images/image1.webp"
+              src="src/assets/images/image2.jpg"
               alt=""
             />
             <p>Greg hooper</p>
